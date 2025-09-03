68e0aaa4cfe21efa4ab59f60425eff96cb271ed4
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2025-09-03 23:10:09 +0500

    Time: 143 ms (26.01%) | Memory: 236.4 MB (19.67%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     bool helper(int idx, int k, vector<int>& stones, vector<vector<int>>& dp) {
-        if(idx == stones.size() - 1) return true;
-        if(k <= 0) return false;
-        if(dp[idx][k] == 1) return true;
-        if(dp[idx][k] == 0) return false;
-        for(int i = idx; i < stones.size(); i++) {
-            if(stones[idx] + k < stones[i]) {
-                dp[idx][k] = 0;
-                return false;
-            }
-            if(stones[idx] + k == stones[i]) {
-                dp[i][k - 1] = helper(i, k - 1, stones, dp);
-                dp[i][k] = helper(i, k, stones, dp);
-                dp[i][k + 1] = helper(i, k + 1, stones, dp);
-                return (max(dp[i][k - 1],max( dp[i][k], dp[i][k + 1]))) ? true : false;
-            }
-        }
-        dp[idx][k] = 0;
+        if(idx == stones.size() - 1) return 1;
+        if(idx >= stones.size() || k <= 0) return 0;
+        if(dp[idx][k] != -1) return dp[idx][k];
+        for(int i = idx + 1; i < stones.size(); i++) 
+            if(stones[idx] + k == stones[i]) 
+                return dp[i][k] = helper(i, k - 1, stones, dp) || helper(i, k, stones, dp) || helper(i, k + 1, stones, dp); 
         return false;
     }
 
     bool canCross(vector<int>& stones) {
-        if (stones[1] > 1)
-            return false;
-        vector<vector<int>> dp(stones.size(),
-                               vector<int>(stones.size() + 1, -1));
+        vector<vector<int>> dp(stones.size(), vector<int>(stones.size(), -1));
         return helper(0, 1, stones, dp);
     }
 };
