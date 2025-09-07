1a175dbb071801aaea825cd118d28e8f7c5aace5
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2025-09-07 10:38:50 +0500

    Time: 1 ms (68.23%) | Memory: 9.2 MB (21.66%) - LeetSync

---
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void helper(int n, int col, vector<bool>& rows, vector<bool>& cols,
+                vector<bool>& md, vector<bool>& ad, vector<string>& temp,
+                vector<vector<string>>& ans) {
+        if (col == n) {
+            ans.push_back(temp);
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (!rows[i] && !cols[col] && !ad[i - col + n - 1] && !md[col + i]) {
+                temp[i][col] = 'Q';
+                rows[i] = 1, cols[col] = 1, ad[i - col + n - 1] = 1, md[i + col] = 1;
+                helper(n, col + 1, rows, cols, md, ad, temp, ans);
+                temp[i][col] = '.';
+                rows[i] = cols[col] = false;
+                ad[i - col + n - 1] = md[i + col] = false;
+            }
+        }
+    }
+
+    int totalNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<bool> rows(n, 0);
+        vector<bool> cols(n, 0);
+        vector<bool> md(2*n, 0);
+        vector<bool> ad(2*n, 0);
+        vector<string> temp(n, string(n, '.'));
+        helper(n, 0, rows, cols, md, ad, temp, ans);
+        return ans.size();
+    }
+};
