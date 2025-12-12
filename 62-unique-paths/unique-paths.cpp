6c6536fe3cb15683b006a24e4aa8ff588af6cd59
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2025-12-12 11:07:23 +0500

    Time: 3 ms (8.79%) | Memory: 9.2 MB (66.03%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
+    int fn(int i, int j, int m, int n, vector<vector<int>> &dp) {
+        if(i == m || j == n) return 0;
+        if(i == m - 1 && j == n - 1) return 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = fn(i + 1, j, m, n, dp) + fn(i, j + 1, m, n, dp);
+    }
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        dp[m-1][n-1] = 1;
-        for(int i = m - 2; i >= 0; i--) 
-            for(int j = n - 2; j >= 0; j--) 
-                dp[i][j] = dp[i+1][j] + dp[i][j+1];
-        return dp[0][0];
+       vector<vector<int>> dp(m, vector<int>(n, -1));
+       return fn(0, 0, m, n, dp);
     }
 };
