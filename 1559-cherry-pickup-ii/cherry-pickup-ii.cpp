f06cc1e29bef2a2f60913fc638c9a168eeda3e32
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2025-12-12 12:59:23 +0500

    Time: 51 ms (50.54%) | Memory: 20.9 MB (52.10%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -3,20 +3,15 @@ public:
     long long f(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
         int n = grid.size();
         int m = grid[0].size();
-
         if (j1 < 0 || j2 < 0 || j1 >= m || j2 >= m)
             return -1e9;
-
         if (i == n-1) {
             if (j1 == j2) return grid[i][j1];
             return grid[i][j1] + grid[i][j2];
         }
-
         if (dp[i][j1][j2] != -1)
             return dp[i][j1][j2];
-
         long long maxi = -1e9;
-
         for (int a = -1; a <= 1; a++) {
             for (int b = -1; b <= 1; b++) {
                 long long value = 0;
@@ -28,7 +23,6 @@ public:
                 maxi = max(maxi, value);
             }
         }
-
         return dp[i][j1][j2] = maxi;
     }
 
