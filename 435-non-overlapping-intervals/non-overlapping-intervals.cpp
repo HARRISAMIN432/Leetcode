03d186b945c53d395827276e9a8904ea54ef9857
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2026-01-28 13:58:57 +0500

    Time: 41 ms (69.49%) | Memory: 94 MB (36.94%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -6,11 +6,10 @@ public:
         int ans = 0;
         int end = intervals[0][1];
         for(int i = 1; i < intervals.size(); i++) {
-            if(intervals[i][0] < end) { 
+            if(intervals[i][0] < end) 
                 ans++;
-            } else {
+            else 
                 end = intervals[i][1]; 
-            }
         }
         return ans;
     }
