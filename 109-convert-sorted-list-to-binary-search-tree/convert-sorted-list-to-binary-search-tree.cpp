ae1726f7f38f6df6ae4b0a4dda6b35172f9dffa2
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2026-01-29 17:13:03 +0500

    Time: 7 ms (27.84%) | Memory: 33.5 MB (12.98%) - LeetSync

---
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bst(int l, int r, vector<int> &nums) {
+        if (l > r) return nullptr;
+        int mid = (l + r) >> 1;
+        TreeNode* node = new TreeNode(nums[mid]);
+        node->left = bst(l, mid - 1, nums);
+        node->right = bst(mid + 1, r, nums);
+        return node;
+    }
+
+    TreeNode* sortedListToBST(ListNode* head) {
+        if(!head) return NULL;
+        vector<int> nums;
+        ListNode* tmp = head;
+        while(tmp) {
+            nums.push_back(tmp->val);
+            tmp = tmp->next;
+        }
+        return bst(0, nums.size() - 1, nums);
+    }
+};
