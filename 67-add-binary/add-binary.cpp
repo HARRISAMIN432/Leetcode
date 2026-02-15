c645c26f7349ab3a5717903dedc1afa811f00d76
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2026-02-15 16:52:36 +0500

    Time: 0 ms (100.00%) | Memory: 9 MB (59.48%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+        string ans;
+        bool carry = 0;
+        for(int i = 0; i < max(a.size(), b.size()); i++) {
+            if(i >= a.size() || i >= b.size()) {
+                if(carry) 
+                    if(i >= a.size()) {
+                        if(b[i] == '0') {
+                            ans += '1';
+                            carry = 0;
+                        }
+                        else ans += '0';
+                    }
+                    else {
+                        if(a[i] == '0') {
+                            ans += '1';
+                            carry = 0;
+                        }
+                        else ans += '0';
+                    }
+                else if(i >= a.size()) ans += b[i];
+                else ans += a[i];
+            } 
+            else 
+                if(a[i] == '0' && b[i] == '0') 
+                    if(carry) {
+                        carry = 0;
+                        ans += '1';
+                    }
+                    else ans += '0';
+                else if(a[i] == '1' && b[i] == '0') 
+                    if(carry) ans += '0';
+                    else ans += '1';
+                else if(a[i] == '0' && b[i] == '1') 
+                    if(carry) ans += '0';
+                    else ans += '1';
+                else {
+                    if(carry) ans += '1';
+                    else {
+                        ans += '0';
+                        carry = 1;
+                    }
+                }
+        }
+        if(carry) ans += '1';
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
