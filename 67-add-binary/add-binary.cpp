735579acdfcdea66265eee9ba522688625e4c8bf
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2026-02-15 17:03:29 +0500

    Time: 0 ms (100.00%) | Memory: 9 MB (36.83%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -8,41 +8,19 @@ public:
         for(int i = 0; i < max(a.size(), b.size()); i++) 
             if(i >= a.size() || i >= b.size()) 
                 if(carry) 
-                    if(i >= a.size()) {
-                        if(b[i] == '0') {
-                            ans += '1';
-                            carry = 0;
-                        }
+                    if(i >= a.size()) 
+                        if(b[i] == '0') ans += '1', carry = 0;
                         else ans += '0';
-                    }
-                    else {
-                        if(a[i] == '0') {
-                            ans += '1';
-                            carry = 0;
-                        }
+                    else 
+                        if(a[i] == '0') ans += '1', carry = 0;
                         else ans += '0';
-                    }
                 else if(i >= a.size()) ans += b[i];
                 else ans += a[i];
             else 
-                if(a[i] == '0' && b[i] == '0') 
-                    if(carry) {
-                        carry = 0;
-                        ans += '1';
-                    }
-                    else ans += '0';
-                else if(a[i] == '1' && b[i] == '0') 
-                    if(carry) ans += '0';
-                    else ans += '1';
-                else if(a[i] == '0' && b[i] == '1') 
-                    if(carry) ans += '0';
-                    else ans += '1';
-                else 
-                    if(carry) ans += '1';
-                    else {
-                        ans += '0';
-                        carry = 1;
-                    }
+                if(a[i] == '0' && b[i] == '0') (carry) ? carry = 0, ans += '1' : ans += '0';
+                else if(a[i] == '1' && b[i] == '0') (carry) ? ans += '0' : ans += '1';
+                else if(a[i] == '0' && b[i] == '1') (carry) ? ans += '0' : ans += '1';
+                else (carry) ? ans += '1' : ans += '0', carry = 1;
         if(carry) ans += '1';
         reverse(ans.begin(), ans.end());
         return ans;
