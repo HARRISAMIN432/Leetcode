0451274ad4fb97701756ba41465eb707d3c42557
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2026-02-17 23:36:47 +0500

    Time: 179 ms (93.95%) | Memory: 10.3 MB (93.64%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int n, m;
+
+    bool dfs(int i, int j, int k, vector<vector<char>>& board, string& word) {
+        if (k == word.size()) return true;
+        if (i < 0 || j < 0 || i >= n || j >= m) return false;
+        if (board[i][j] != word[k]) return false;
+        char temp = board[i][j];
+        board[i][j] = '#';  
+        bool found = dfs(i + 1, j, k + 1, board, word) || dfs(i - 1, j, k + 1, board, word) || dfs(i, j + 1, k + 1, board,    word) || dfs(i, j - 1, k + 1, board, word);
+        board[i][j] = temp;  
+        return found;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        n = board.size();
+        m = board[0].size();
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < m; j++) 
+                if (dfs(i, j, 0, board, word))
+                    return true;
+        return false;
+    }
+};
