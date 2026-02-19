fcde8becc176366567109d2d08fb5d8b9ad275ce
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2026-02-19 23:02:19 +0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (29.05%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
-    int f(int n, vector<int> &dp) {
-        if (n == 1) return 1;   
-        if (dp[n] != -1) return dp[n];
-        int ans = 0;
-        for (int i = 1; i < n; i++) ans = max(ans, max(i * (n - i), i * f(n - i, dp)));
-        return dp[n] = ans;
-    }
-
     int integerBreak(int n) {
         vector<int> dp(n + 1, 0);
-        dp[1] = 1, dp[0] = 0;
-        for (int i = 2; i <= n; i++) {
-            for (int j = 1; j < i; j++) {
-                dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
-            }
-        }
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) for (int j = 1; j < i; j++) dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
         return dp[n];
     }
 };
