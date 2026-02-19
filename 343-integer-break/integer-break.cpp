be79bcf380350557f7c22418307de38bbec8b4da
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2026-02-19 23:01:35 +0500

    Time: 0 ms (100.00%) | Memory: 8.7 MB (29.05%) - LeetSync

--- 343-integer-break/integer-break.cpp
+++ 343-integer-break/integer-break.cpp
@@ -9,7 +9,13 @@ public:
     }
 
     int integerBreak(int n) {
-        vector<int> dp(n + 1, -1);
-        return f(n, dp);
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1, dp[0] = 0;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
     }
 };
