36aa685ce31556fb1f36506e1ebe00ac6e197563
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2026-02-22 11:11:02 +0500

    Time: 0 ms (100.00%) | Memory: 8.1 MB (44.66%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int ans = 0, prev = -1;
+        for(int i = 0; i < 32; i++) 
+            if((n & (1 << i))) {
+                if(prev != -1) ans = max(ans, i - prev);
+                prev = i;
+            } 
+        return ans;
+    }
+};
