6ac56626182ca5524f0cf58057c2f3deac90ab08
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2026-02-25 16:30:57 +0500

    Time: 343 ms (5.24%) | Memory: 64.7 MB (5.01%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -1,11 +1,47 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        int size = 1 << n; 
-        vector<int> res;
-        for(int i = 0; i < size; i++) {
-            res.push_back(i ^ (i >> 1));
+        unordered_set<string> dp;
+        vector<int> ans = {0};
+        string s;
+        for(int i = 0; i < n; i++) s += '0';
+        dp.insert(s);
+        f(n, s, ans, dp);
+        return ans;
+    }
+
+private:
+    void f(int n, string &s, vector<int> &ans, unordered_set<string> &dp) {
+        for(int i = s.size() - 1; i >= 0; i--) {
+            if(s[i] == '0') {
+                s[i] = '1';
+                if(dp.find(s) != dp.end()) {
+                    s[i] = '0';
+                    continue;
+                }
+                ans.push_back(binToDec(s));
+                dp.insert(s);
+                f(n, s, ans, dp);
+            }
+            else {
+                s[i] = '0';
+                if(dp.find(s) != dp.end()) {
+                    s[i] = '1';
+                    continue;
+                }
+                ans.push_back(binToDec(s));
+                dp.insert(s);
+                f(n, s, ans, dp);
+            }
         }
-        return res;
+    }
+
+    int binToDec(string s) {
+        int ans = 0, power = 0;
+        for(int i = s.size() - 1; i >= 0; i--) {
+            if(s[i] == '1') ans += pow(2, power);
+            power++;
+        }
+        return ans;
     }
 };
