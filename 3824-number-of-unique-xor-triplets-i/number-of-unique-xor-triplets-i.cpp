0171cb3b39cd03331332417981848a9d166de78a
Muhammad Harris Amin <144408408+HARRISAMIN432@users.noreply.github.com>
2026-04-10 10:15:27 +0500

    Time: 0 ms (100.00%) | Memory: 199 MB (27.48%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -3,7 +3,6 @@ public:
     int uniqueXorTriplets(vector<int>& nums) {
         int n =  nums.size();
         if(n <= 2) return n;
-        int bits = floor(log2(n)) + 1;
-        return pow(2, bits);
+        return pow(2, floor(log2(n)) + 1);
     }
 };
