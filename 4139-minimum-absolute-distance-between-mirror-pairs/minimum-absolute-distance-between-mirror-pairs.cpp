2f06e23f57066a9c5fac333105c3bf1e5fa03ff8
Muhammad Harris Amin <144408408+HARRISAMIN432@users.noreply.github.com>
2026-04-17 13:03:54 +0500

    Time: 130 ms (46.57%) | Memory: 131.7 MB (90.69%) - LeetSync

---
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int reverseNum(int x) {
+        int rev = 0;
+        while(x > 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
+        }
+        return rev;
+    }
+
+    int minMirrorPairDistance(vector<int>& nums) {
+        unordered_map<int,int> lastPos;
+        int ans = INT_MAX;
+        for(int j = 0; j < nums.size(); j++) {
+            if(lastPos.count(nums[j])) 
+                ans = min(ans, j - lastPos[nums[j]]);
+            int rev = reverseNum(nums[j]);
+            lastPos[rev] = j;
+        }
+
+        return ans == INT_MAX ? -1 : ans;
+    }
+};
