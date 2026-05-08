ceb7369a3ead6d3240f583245f3212aece83a656
Muhammad Harris Amin <144408408+HARRISAMIN432@users.noreply.github.com>
2026-05-08 14:43:37 +0500

    Time: 16 ms (42.17%) | Memory: 11.4 MB (37.60%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, -1));
-        return f(n, k, target, dp);
-    }
-
-private:
-    const int MOD = 1e9 + 7;
-
-    int f(int n ,int k, int target, vector<vector<int>> &dp) {
-        if(target < 0) return 0;
-        if(n == 0) return target == 0;
-        if(dp[n][target] != -1) return dp[n][target];
-        int ways = 0;
-        for(int i = 1; i <= k; i++) 
-            ways = (ways + f(n - 1, k, target - i, dp)) % MOD;
-        return dp[n][target] = ways % MOD;
+    int numRollsToTarget(int n, int f, int target) {
+        vector<vector<int>> dp(n + 1, vector<int>(target + 2, 0));
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= target; j++) {
+                int ways = 0;
+                for(int k = 1; k <= f; k++) 
+                    if(j - k >= 0) ways = (ways + dp[i - 1][j - k]) % 1000000007;
+                dp[i][j] = ways % 1000000007;
+            }
+        }
+        return dp[n][target];
     }
 };
