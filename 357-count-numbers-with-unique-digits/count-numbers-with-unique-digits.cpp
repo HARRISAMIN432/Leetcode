e3020fdb3b4a9ddd2ba84b7bcf46506d443aa5d1
Muhammad Harris Amin <144408408+HARRISAMIN432@users.noreply.github.com>
2026-06-23 15:26:45 +0500

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.33%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -3,9 +3,8 @@ public:
     int countNumbersWithUniqueDigits(int n) {
         if (n == 0) return 1;
         int ans = 10, u = 9, av = 9;
-        for (int len = 2; len <= n && av > 0; len++) {
+        for (int len = 2; len <= n && av > 0; len++) 
             u *= av, ans += u, av--;
-        }
         return ans;
     }
 };
